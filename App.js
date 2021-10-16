261b90bc725e29aff22d440f1839bca0f3038912
Abdul Rahman <abdul.rahman95@hotmail.com>
2021-10-16 05:45:51 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+import Bottomtab from './src/Navigation/Bottomtab';
+import Home from './src/Screens/Home';
+
+const App =  () => {
+  return (
+//  <Home/>
+<Bottomtab/>
+  );
+};
+
+
+export default App;
